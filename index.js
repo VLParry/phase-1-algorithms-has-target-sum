a05dc2a9bfe8cfb5170af32a276fd1fa9a9cd699
Victoria Parry <vlparry@gmail.com>
2022-05-31 12:54:33 -0400

    commit tree 93ac65506d2d0385bc30def9508841d8ddd50cd3

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
+// function hasTargetSum(array, target) {
+//   // Write your algorithm here
+//   //   for (let i = 0; i < array.length; i++) {
+//   //     let cNum = array[i]; //my current number
+//   //     for (let j = i; j < array.length; j++) {
+//   //       if (cNum !== array[j] && cNum + array[j] === target) {
+//   //         let pair = {};
+//   //         pair.key1 = cNum;
+//   //         pair.key2 = array[j];
+//   //         console.log(pair);
+//   //       }
+
+//   //     }
+//   //   }
+//   // }
+
+//   const lookup = {};
+
+//   for (let i = 0; i < array.length; i++) {
+//     const num = array[i];
+
+//     if (lookup[num] === undefined) {
+//       lookup[num] = i;
+//     }
+
+//     if (lookup[target - num] !== undefined && lookup[target - num] !== i) {
+//       return [target - num, num];
+//     }
+//   }
+
+//   return undefined;
+// }
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i]
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true
+    }
+  }
+  return false
 }
+
+
+
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,9 +52,12 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
-*/
+// add the first number in the array to the last number to the last number
+Compare solution to the target integer
+If they equal one another, stop
+If they dont equal one another, add the first number in the array to the second to last number. and so on, until finding a match.
 
-/*
+// /*
   Add written explanation of your solution here
 */
 
